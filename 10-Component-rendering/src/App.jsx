37f05c80460f68e2f39e154e3f0686fa7ec229f2
vijaybhadade <vijaybhadade01@gmail.com>
2026-09-22 13:37:56 +0530

    add base layout and entry point

---
+++ 10-Component-rendering/src/App.jsx
@@ -0,0 +1,15 @@
+import SwitchCase from "./components/SwitchCase.jsx";
+import Home from "./components/Home";
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Home/> 
+        </>
+  )
+}
+
+export default App
