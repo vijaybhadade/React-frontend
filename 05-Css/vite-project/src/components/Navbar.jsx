529cbb5313638f47ab06f974621061ebec57bf0b
vijaybhadade <vijaybhadade01@gmail.com>
2026-09-22 12:43:56 +0530

    add changes

---
+++ 05-Css/vite-project/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+function Navbar()
+{
+    return(
+        <>
+         <h1 className="bg-blue-400">Heading</h1>
+         <button className="bg-red-500 ">click</button>
+        </>
+    );
+}
+
+export default Navbar;
